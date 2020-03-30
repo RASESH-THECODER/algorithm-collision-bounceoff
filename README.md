e7c45dded46d2db5c726e3324b561ed1dc46da60
RASESH-THECODER <62789714+RASESH-THECODER@users.noreply.github.com>
2020-03-30 12:44:39 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# algorithm-collision-bounceoff
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,46 @@
+var fr,mr;
+
+function setup() {
+  createCanvas(800,400);
+ fr= createSprite(200,200,50,80);
+ mr=createSprite(400,200,80,30);
+ fr.shapeColor=("green");
+ mr.shapeColor=("green");
+ mr.velocityY=-5;
+ fr.velocityY=5;
+}
+function draw() {
+  background(255,255,255); 
+  mr.x=World.mouseX;
+  mr.y=World.mouseY;
+  
+  if(mr.x-fr.x<fr.width/2+mr.width/2
+    && fr.x-mr.x<fr.width/2+mr.width/2){
+      mr.velocityX=mr.velocityX*(-1);
+      fr.velocityX=fr.velocityX*(-1);
+
+    }
+   
+if(mr.y-fr.y<fr.height/2+mr.height/2
+    && fr.y-mr.y<fr.height/2+mr.height/2){
+      mr.velocityY=mr.velocityY*(-1);
+      fr.velocityY=fr.velocityY*(-1);
+      
+    }
+  
+  /*if(mr.x-fr.x<fr.width/2+mr.width/2
+  && fr.x-mr.x<fr.width/2+mr.width/2
+  &&mr.y-fr.y<fr.height/2+mr.height/2
+    && fr.y-mr.y<fr.height/2+mr.height/2){
+      fr.shapeColor=("black");
+      mr.shapeColor=("black");
+        
+    }
+  else{
+    fr.shapeColor=("green");
+ mr.shapeColor=("green");
+
+  }*/
+  drawSprites();
+  
+}
