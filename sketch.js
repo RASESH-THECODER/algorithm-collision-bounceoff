381c5a14e111c5c245083f4074797b5b3b59fb1b
RASESH-THECODER <62789714+RASESH-THECODER@users.noreply.github.com>
2020-04-06 20:34:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,46 +1,30 @@
-var fr,mr;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-function setup() {
-  createCanvas(800,400);
- fr= createSprite(200,200,50,80);
- mr=createSprite(400,200,80,30);
- fr.shapeColor=("green");
- mr.shapeColor=("green");
- mr.velocityY=-5;
- fr.velocityY=5;
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+  box1=new Box(200,300,50,50);
+  box2=new Box(240,100,50,100);  
+  box3=new Box(100,250,40,40);
+  box4=new Box(150,200,60,200);
+  box5=new Box(250,220,200,50)
+  ground=new Ground(200,390,400,20);
 }
-function draw() {
-  background(255,255,255); 
-  mr.x=World.mouseX;
-  mr.y=World.mouseY;
-  
-  if(mr.x-fr.x<fr.width/2+mr.width/2
-    && fr.x-mr.x<fr.width/2+mr.width/2){
-      mr.velocityX=mr.velocityX*(-1);
-      fr.velocityX=fr.velocityX*(-1);
 
-    }
-   
-if(mr.y-fr.y<fr.height/2+mr.height/2
-    && fr.y-mr.y<fr.height/2+mr.height/2){
-      mr.velocityY=mr.velocityY*(-1);
-      fr.velocityY=fr.velocityY*(-1);
-      
-    }
-  
-  /*if(mr.x-fr.x<fr.width/2+mr.width/2
-  && fr.x-mr.x<fr.width/2+mr.width/2
-  &&mr.y-fr.y<fr.height/2+mr.height/2
-    && fr.y-mr.y<fr.height/2+mr.height/2){
-      fr.shapeColor=("black");
-      mr.shapeColor=("black");
-        
-    }
-  else{
-    fr.shapeColor=("green");
- mr.shapeColor=("green");
-
-  }*/
-  drawSprites();
-  
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+   box2.display();
+   box3.display();
+   box4.display(); 
+   box5.display(); 
+   ground.display();
 }
